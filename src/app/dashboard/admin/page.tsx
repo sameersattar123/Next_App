899994cba9358c56250a7eb4b90a3bb3f7e1c20e
Sameer <sameersattar1111@gmail.com>
2024-08-28 08:43:29 +0500

    event calendar page complete

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,33 +1,38 @@
-import AttendenceChart from "@/components/AttendenceChart"
-import CountChart from "@/components/CountChart"
-import FianceChart from "@/components/FianceChart"
-import UserCard from "@/components/UserCard"
+import Announcements from "@/components/Announcements";
+import AttendenceChart from "@/components/AttendenceChart";
+import CountChart from "@/components/CountChart";
+import EventCalendar from "@/components/EventCalendar";
+import FianceChart from "@/components/FianceChart";
+import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
   return (
     <div className="flex flex-col md:flex-row gap-4  p-4 h-screen">
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
         <div className="flex gap-4 justify-center flex-wrap">
-        <UserCard type="parents" />
-        <UserCard type="students" />
-        <UserCard type="stuffs" />
-        <UserCard type="teachers" />
+          <UserCard type="parents" />
+          <UserCard type="students" />
+          <UserCard type="stuffs" />
+          <UserCard type="teachers" />
         </div>
         <div className="flex flex-col lg:flex-row gap-4">
           <div className="w-full lg:w-1/3 h-[450px]">
-            <CountChart/>
+            <CountChart />
           </div>
           <div className="w-full lg:w-2/3 h-[450px]">
-          <AttendenceChart/>
+            <AttendenceChart />
           </div>
         </div>
         <div className="w-full h-[500px]">
-          <FianceChart/>
+          <FianceChart />
         </div>
       </div>
-      <div className="w-full lg:w-1/3 bg-emerald-300">right</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPage
+export default AdminPage;
--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,11 +1,59 @@
-import React from 'react'
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+// TEMPORARY
+const events = [
+  {
+    id: 1,
+    title: "Lorem ipsum dolor",
+    time: "12:00 PM - 2:00 PM",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 2,
+    title: "Lorem ipsum dolor",
+    time: "12:00 PM - 2:00 PM",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 3,
+    title: "Lorem ipsum dolor",
+    time: "12:00 PM - 2:00 PM",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+];
 
 const EventCalendar = () => {
+  const [value, onChange] = useState<Value>(new Date());
   return (
-    <div>
-      EventCalendar
+    <div className="bg-white p-4 rounded-md">
+      <Calendar onChange={onChange} value={value} />
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl my-4 font-semibold ">Events</h1>
+        <Image height={20} width={20} src="/moreDark.png" alt="" />
+      </div>
+      <div className="">
+        {events.map((event) => {
+          return (
+            <div className="p-5 rounded-md border-2 border-gray-100 odd:border-t-lamaSky even:border-t-lamaYellow" key={event.id}>
+              <div className="flex items-center justify-between">
+                <h1 className="font-semibold text-gray-600">{event.title}</h1>
+                <span className="text-xs text-gray-300">{event.time}</span>
+              </div>
+              <p className="mt-2 text-gray-400 text-sm">{event.description}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EventCalendar
+export default EventCalendar;
