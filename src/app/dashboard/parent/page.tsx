aaf53dd567e536e63c47e027a238c720668f9cb8
Sameer <sameersattar1111@gmail.com>
2024-08-29 07:31:14 +0500

    complete

--- src/app/dashboard/parent/page.tsx
+++ src/app/dashboard/parent/page.tsx
@@ -1,11 +1,22 @@
+import Announcements from "@/components/Announcements"
+import BigCalendar from "@/components/BigCalendar"
 
 const ParentPage = () => {
-    return (
-      <div>
-       ParentPage
+  return (
+    <div className="flex flex-col p-4 gap-4 xl:flex-row">
+      <div className="w-full xl:w-2/3 ">
+      <div className="h-full p-4  bg-white rounded-md">
+        <h1 className="text-xl font-semibold">Schedule (Sameer Sattar)</h1>
+        <BigCalendar/>
       </div>
-    )
-  }
+      </div>
+      <div className='w-full xl:w-1/3 flex flex-col gap-8'>
+        <Announcements/>
+      </div>
+    </div>
+  )
+}
+
   
   export default ParentPage
   
--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,11 +1,9 @@
 'use client';
-
-
-import { useState } from 'react'
 import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar'
-import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from 'moment'
 import { calendarEvents } from '@/lib/data'
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useState } from 'react'
 
 const localizer = momentLocalizer(moment)
 
@@ -24,7 +22,7 @@ const BigCalendar = () => {
       endAccessor="end"
       views={["work_week", "day"]}
       view={view}
-      style={{ height: "98" }}
+      style={{ height: "98%" }}
       onView={handleOnChangeView}
       min={new Date(2025, 1, 0, 8, 0, 0)}
       max={new Date(2025, 1, 0, 17, 0, 0)}
