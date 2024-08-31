572e07b37a80c8b315f21993b72e89aa1971ca2f
Sameer <sameersattar1111@gmail.com>
2024-08-31 23:09:33 +0500

    start

--- src/app/dashboard/list/teachers/page.tsx
+++ src/app/dashboard/list/teachers/page.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+import React from "react";
 
 const TeacherListPage = () => {
   return (
-    <div>
-    
+    <div className="flex flex-col p-4 gap-4 xl:flex-row">
+      <div className="w-full xl:w-2/3 ">
+        <div className="h-full p-4  bg-white rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (Sameer Sattar)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TeacherListPage
+export default TeacherListPage;
